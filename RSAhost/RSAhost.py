963d123daa92a36b0bea518f360e6ffa6fe97bd3
AdityaGawali <adityagawali13@gmail.com>
2018-12-10 17:20:54 +0530

    merged host and client

--- RSAhost/RSAhost.py
+++ RSAhost/RSAhost.py
@@ -38,53 +38,76 @@ def generatePrivateKey(K,phiofn,publicexp): #Generates PrivateKey(D) for decrypt
     D = (K*(phiofn)+1)/publicexp
     return D
 
-def writePublicKey(num,exp,privateDecryptionKey): #write public key
-    publicKeyFile = open("publickey.txt","w")
-    publicKeyFile.write(str(num))
-    publicKeyFile.write("\n")
-    publicKeyFile.write(str(exp))
-    publicKeyFile.write("\n")
-    publicKeyFile.write(str(privateDecryptionKey))
-    publicKeyFile.close()
+# def writePublicKey(num,exp,privateDecryptionKey): #write public key
+#     publicKeyFile = open("publickey.txt","w")
+#     publicKeyFile.write(str(num))
+#     publicKeyFile.write("\n")
+#     publicKeyFile.write(str(exp))
+#     publicKeyFile.write("\n")
+#     publicKeyFile.write(str(privateDecryptionKey))
+#     publicKeyFile.close()
 
 
 
 
-def readCipher(): #read ciphertext 
-    publicKeyFile=open("publickey.txt","r")
-    publicKeyFile.readline() 
-    publicKeyFile.readline()
-    privateDecryptionKey = publicKeyFile.readline()
-    publicKeyFile.close()
-    cipherfile = open("cipher.txt","r")
-    cipherText = cipherfile.readline()
-    return cipherText
+# def readCipher(): #read ciphertext 
+#     publicKeyFile=open("publickey.txt","r")
+#     publicKeyFile.readline() 
+#     publicKeyFile.readline()
+#     privateDecryptionKey = publicKeyFile.readline()
+#     publicKeyFile.close()
+#     cipherfile = open("cipher.txt","r")
+#     cipherText = cipherfile.readline()
+#     return cipherText
 
-def decrypt(cipherInt,privateDecryptionKey,PrimeProduct): # decryption algo
-    messageInt = []
-    for x in cipherInt:
-        messageInt[x] = (cipherInt[x]**privateDecryptionKey)%PrimeProduct    
-    return messageInt
 
-def writeNewMessage(messageText): #write decrypted message
-    messagenew = open("message2.txt","w")
-    messagenew.write(messageText)
-    messagenew.close()
-
-def ConvertInttoString(messageInt): #convert int list to string text
-    messagelist=[chr(x) for x in messageInt]
-    messageText = "".join(messagelist)
+def readMessageText():
+    messagefile = open("message.txt",'r')
+    messageText = messagefile.readline()
+    messagefile.close()
     return messageText
 
-def ConvertStringtoInt(cipherText): #convert string text to int list
-    cipherlist = list(cipherText)
-    cipherInt = [ord(x) for x in cipherlist]
-    return cipherInt   
+def ConvertInttoString(someInt): #convert int list to string text
+    somelist=[chr(x) for x in someInt]
+    someText = "".join(somelist)
+    return someText
+
+def ConvertStringtoInt(someText): #convert string text to int list
+    somelist = list(someText)
+    someInt = [ord(x) for x in somelist]
+    return someInt  
+
+def encrypt(messageText,primeProduct,publicExponentKey):
+    cipherInt=[]
+    messageInt = ConvertStringtoInt(messageText)
+    for x in range(len(messageInt)):
+        cipherInt = ((messageInt**publicExponentKey)%primeProduct)
+    cipherText = ConvertInttoString(cipherInt)
+    return cipherText
+
+def decrypt(cipherText,primeProduct,privateDecryptionKey):
+    cipherInt = ConvertStringtoInt(cipherText)
+    decryptedMessageInt=[]
+    for x in range(len(cipherInt)):
+        decryptedMessageInt[x] = ((cipherInt[x]**privateDecryptionKey)%primeProduct)
+    decryptedMessageText = ConvertInttoString(decryptedMessageInt)
+    return decryptedMessageText
+    
+
 #MAIN CODE STARTS HERE
-primeNumber1 = generatePrimeNumber(1024)
-primeNumber2 = generatePrimeNumber(1024)
-PrimeProduct = productOfPrimeNumber(primeNumber1,primeNumber2)
+primeNumber1 = generatePrimeNumber(128)
+primeNumber2 = generatePrimeNumber(128)
+primeProduct = productOfPrimeNumber(primeNumber1,primeNumber2)
 PHIIofN = Phi(primeNumber1,primeNumber1)
 publicExponentKey = generatePublicKeyExponent(PHIIofN)
 privateDecryptionKey = generatePrivateKey(2,PHIIofN,publicExponentKey)
-writePublicKey(PrimeProduct,publicExponentKey,privateDecryptionKey)
+
+messageText=readMessageText()
+CipherText = encrypt(messageText,primeProduct,publicExponentKey)
+decryptedMessageText = decrypt(CipherText,primeProduct,privateDecryptionKey)
+
+final = open("final.txt","w")
+final.write(CipherText)
+final.write("\n")
+final.write(decryptedMessageText)
+print(decryptedMessageText)
